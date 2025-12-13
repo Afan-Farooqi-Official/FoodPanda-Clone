7037a14d3f649b15ddb07f00306a592c9dec8a61
Afan-Farooqi-Official <affanqaisar000@gmail.com>
2025-12-13 10:21:28 +0500

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -42,13 +42,13 @@ const Footer = () => {
                 </div>
             </div>
             <div class='flex flex-col md:flex-row py-7 items-center justify-between gap-6'>
-                <div class='flex items-center justify-center md:justify-start gap-4'>
+                <div class='flex items-center justify-center md:justify-start md:pl-9 gap-4'>
                     <nav class='flex items-center gap-2 hover:cursor-pointer'>
                         <img class='w-7 h-7' src="../download.svg" alt="logo-pic" />
                         <img class='h-6' src="../download (1).svg" alt="logo-txt" />
                     </nav>
                     <span>|</span>
-                    <img class='ml-5' src="../delivery-hero.svg" alt="" />
+                    <img src="../delivery-hero.svg" alt="" />
                 </div>
                 <div class='flex gap-4 justify-center md:justify-end md:mr-14'>
                     <img class='border border-gray-200 p-2 rounded-full cursor-pointer transition-transform duration-300 ease-out hover:bg-gray-100 hover:scale-110' src="../fb.svg" alt="contact" />
