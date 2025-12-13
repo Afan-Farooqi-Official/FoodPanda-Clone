ef476cbd192b5490c7ab0e0ec3140cff47598b41
Afan-Farooqi-Official <affanqaisar000@gmail.com>
2025-12-13 10:10:50 +0500

    bug fixes

--- src/restaurantsData.jsx
+++ src/restaurantsData.jsx
@@ -1,119 +1,119 @@
 
 const restaurants = [
     {
-        img: "/public/dishes/1.jpg",
+        img: "/dishes/1.jpg",
         dish: "Flavour Shop",
         category: "Pakistani",
         rating: null,
         reviews: null,
     },
     {
-        img: "/public/dishes/2.jpg",
+        img: "/dishes/2.jpg",
         dish: "Al Razzaq Nehari House",
         category: "Pakistani In-Store Price",
         rating: 4.6,
         reviews: '100+'
     },
     {
-        img: "/public/dishes/3.jpg",
+        img: "/dishes/3.jpg",
         dish: "Food Bites",
         category: "Fast Food",
         rating: 4.3,
         reviews: '(48)'
     },
     {
-        img: "/public/dishes/4.jpg",
+        img: "/dishes/4.jpg",
         dish: "Filfilher",
         category: "Fast Food",
         rating: 3.2,
         reviews: '(7)'
     },
     {
-        img: "/public/dishes/5.jpg",
+        img: "/dishes/5.jpg",
         dish: "Hani Home Foods",
         category: "Healthy",
         rating: 3.7,
         reviews: '(6)'
     },
     {
-        img: "/public/dishes/6.jpg",
+        img: "/dishes/6.jpg",
         dish: "Suadish Foods",
         category: "Fast Food",
         rating: 3.7,
         reviews: '(6)'
     },
     {
-        img: "/public/dishes/7.jpg",
+        img: "/dishes/7.jpg",
         dish: "Ghar ka zaaiqa",
         category: null,
         rating: null,
         reviews: null
     },
     {
-        img: "/public/dishes/8.jpg",
+        img: "/dishes/8.jpg",
         dish: "New Al Habib Biryani Center-5B",
         category: null,
         rating: 4.9,
         reviews: '(6)'
     },
     {
-        img: "/public/dishes/9.jpg",
+        img: "/dishes/9.jpg",
         dish: "Sufra By Shazia",
         category: "Pakistani",
         rating: 5,
         reviews: '(500+)'
     },
     {
-        img: "/public/dishes/10.jpg",
+        img: "/dishes/10.jpg",
         dish: "AA2 Food Center",
         category: "Pakistani",
         rating: 3.4,
         reviews: '(500+)'
     },
     {
-        img: "/public/dishes/11.jpg",
+        img: "/dishes/11.jpg",
         dish: "Jamil Haleem House",
         category: "Pakistani",
         rating: 4.9,
         reviews: '(100+)'
     },
     {
-        img: "/public/dishes/12.jpg",
+        img: "/dishes/12.jpg",
         dish: "Home Kitchen",
         category: "Pakistani",
         rating: 4.9,
         reviews: '(500+)'
     },
     {
-        img: "/public/dishes/13.jpg",
+        img: "/dishes/13.jpg",
         dish: "Faazu Paaya Chaany",
         category: "Pakistani",
         rating: 3.9,
         reviews: '(2000+)'
     },
     {
-        img: "/public/dishes/14.jpg",
+        img: "/dishes/14.jpg",
         dish: "ZBK fruit salad bar",
         category: "Desserts",
         rating: 5,
         reviews: '(64)'
     },
     {
-        img: "/public/dishes/15.jpg",
+        img: "/dishes/15.jpg",
         dish: "Bite Bite Fast Food & BBQ",
         category: "Fast Food",
         rating: 4.5,
         reviews: '(500+)'
     },
     {
-        img: "/public/dishes/16.jpg",
+        img: "/dishes/16.jpg",
         dish: "Syeda Food Kitchen",
         category: "Chinese",
         rating: 4.5,
         reviews: '(27)'
     },
     {
-        img: "/public/dishes/18.jpg",
+        img: "/dishes/18.jpg",
         dish: "MashaAllah Shirmal House",
         category: "Pakistani",
         rating: 5,
